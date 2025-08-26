d9859a8ce2f03414a709e060052b96402c09601c
Narngisa <nattanan43658@gmail.com>
2025-08-27 03:21:23 +0700

    test(Home)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,4 +5,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: true, // ถ้าต้องการเข้าจาก LAN / ngrok
+    port: 3000, // port ของคุณ
+    strictPort: true,
+    allowedHosts: ['2fb411a9646e.ngrok-free.app']
+  }
 })
